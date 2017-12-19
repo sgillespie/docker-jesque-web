bb83fb633f93bbad05273d50720be0f59665b8d5
Sean Gillespie <sean@mistersg.net>
2017-12-19 09:28:56 -0500

    Allow users to override webapp context

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export JETTY_CONTEXT=${JETTY_CONTEXT:-/}
 export REDIS_HOST=${REDIS_HOST:-localhost}
 export REDIS_PORT=${REDIS_PORT:-6379}
 export REDIS_TIMEOUT=${REDIS_TIMEOUT:-5000}
@@ -8,8 +9,14 @@ export REDIS_NAMESPACE=${REDIS_NAMESPACE:-resque}
 export REDIS_DATABASE=${REDIS_DATABASE:-0}
 
 # Generate redis.properties
-CONFIG_ROOT="${JETTY_BASE}/webapps/ROOT/WEB-INF/classes/META-INF/spring"
-envsubst < "${CONFIG_ROOT}/redis.properties.tmpl" > "${CONFIG_ROOT}/redis.properties"
+CONFIG_ROOT="/jesque-web/WEB-INF/classes/META-INF/spring"
+envsubst < "${CONFIG_ROOT}/redis.properties.tmpl" \
+    > "${CONFIG_ROOT}/redis.properties"
+
+# Generate jetty context file
+WEBAPP_ROOT="${JETTY_BASE}/webapps"
+envsubst < "${WEBAPP_ROOT}/jesque-web.xml.tmpl" \
+    > "${WEBAPP_ROOT}/jesque-web.xml"
 
 # Run the jetty image's entrypoint
 exec /docker-entrypoint.sh
